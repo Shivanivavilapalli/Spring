8ecb19e3c71efc25bbbf7edc1ddedab09bc78966
SHIVANI <153481353+Shivanivavilapalli@users.noreply.github.com>
2026-06-16 17:42:34 +0530

    Add files via upload

---
+++ sp/src/main/java/sp/Myservlet.java
@@ -0,0 +1,32 @@
+package sp;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import java.io.IOException;
+import javax.servlet.Servlet;
+
+/**
+ * Servlet implementation class Myservlet
+ */
+public class Myservlet extends GenericServlet implements Servlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see GenericServlet#GenericServlet()
+     */
+    public Myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#service(ServletRequest request, ServletResponse response)
+	 */
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
